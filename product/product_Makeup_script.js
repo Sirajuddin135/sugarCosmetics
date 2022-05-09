13586bf846abf7c6afba9aeb583e1249cb7fe621
kanadOncode-141 <kbbiswas01.hl@gmail.com>
2022-05-09 12:48:38 +0530

    all nav and footer function added

--- product/product_Makeup_script.js
+++ product/product_Makeup_script.js
@@ -401,5 +401,39 @@ card.forEach(function(elem) {
 
 
 
+// ---------------------------------nav bar ---------------------------------------
 
+document.querySelector("#cross").addEventListener("click", myfunction);
+
+ function myfunction(){
+   
+    document.querySelector("#offertag").remove()
+    
+  
+ }
+ var userDetailArr=JSON.parse(localStorage.getItem("userData"))
+ userDetailArr.forEach(function(el){
+    if(userDetailArr.length!=0){
+  document.querySelector('#loginanch').innerHTML="Hi,"+el.firstname
+ }
+ });
+// ---------------------------------footer or bottom -----------------------------------------
+
+document.querySelector("button").addEventListener("click", redirect)
+
+function redirect() {
+    var mail = document.querySelector("#newsletter_footer").value
+    if (mail == "") {
+        var delay = 2000;
+
+        document.querySelector("#error_footer").innerHTML = "please enter valid email"
+
+        setTimeout(function () {
+            window.location.reload();
+        }, delay);
+    }
+    else {
+        window.location.href = "../login pages/login.html";
+    }
+}
 
--- product/product_skincare_script.js
+++ product/product_skincare_script.js
@@ -284,3 +284,41 @@ card.forEach(function(elem) {
     document.querySelector("#card_container").append(data);
 });
 
+
+
+// ---------------------------------nav bar ---------------------------------------
+
+document.querySelector("#cross").addEventListener("click", myfunction);
+
+ function myfunction(){
+   
+    document.querySelector("#offertag").remove()
+    
+  
+ }
+ var userDetailArr=JSON.parse(localStorage.getItem("userData"))
+ userDetailArr.forEach(function(el){
+    if(userDetailArr.length!=0){
+  document.querySelector('#loginanch').innerHTML="Hi,"+el.firstname
+ }
+ });
+
+// ---------------------------------footer -----------------------------------------
+
+document.querySelector("button").addEventListener("click", redirect)
+
+function redirect() {
+    var mail = document.querySelector("#newsletter_footer").value
+    if (mail == "") {
+        var delay = 2000;
+
+        document.querySelector("#error_footer").innerHTML = "please enter valid email"
+
+        setTimeout(function () {
+            window.location.reload();
+        }, delay);
+    }
+    else {
+        window.location.href = "../login pages/login.html";
+    }
+}
